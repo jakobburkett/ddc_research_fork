70cb2c8503642a728d1c68e1339ccba6adf87511
Jakob Burkett <jb410817@ohio.edu>
2024-04-04 13:17:11 -0400

    started walkthrough and small changes to 1 and 2 for learning

--- ddc/scripts/sml_sym_1_chart.sh
+++ ddc/scripts/sml_sym_1_chart.sh
@@ -1,6 +1,6 @@
 source sml_0_push.sh
 
-python2 create_charts.py \
+python create_charts.py \
 	${SMDATA_DIR}/json_filt/${1}_train.txt \
 	${SMDATA_DIR}/json_filt/${1}_valid.txt \
 	${SMDATA_DIR}/json_filt/${1}_test.txt \
